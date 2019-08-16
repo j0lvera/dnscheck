2eb004cbfe8b177f56775cf74852162490bc118a
j0lv3r4 <juan@jolvera.dev>
2019-08-16 18:06:34 -0500

    first commit

---
+++ index.py
@@ -0,0 +1,109 @@
+__version__ = "0.1.0"
+
+from bottle import Bottle, template, request, redirect
+import dns.resolver
+
+app = Bottle()
+
+
+ids = [
+    "NONE",
+    "A",
+    "NS",
+    "MD",
+    "MF",
+    "CNAME",
+    "SOA",
+    "MB",
+    "MG",
+    "MR",
+    "NULL",
+    "WKS",
+    "PTR",
+    "HINFO",
+    "MINFO",
+    "MX",
+    "TXT",
+    "RP",
+    "AFSDB",
+    "X25",
+    "ISDN",
+    "RT",
+    "NSAP",
+    "NSAP-PTR",
+    "SIG",
+    "KEY",
+    "PX",
+    "GPOS",
+    "AAAA",
+    "LOC",
+    "NXT",
+    "SRV",
+    "NAPTR",
+    "KX",
+    "CERT",
+    "A6",
+    "DNAME",
+    "OPT",
+    "APL",
+    "DS",
+    "SSHFP",
+    "IPSECKEY",
+    "RRSIG",
+    "NSEC",
+    "DNSKEY",
+    "DHCID",
+    "NSEC3",
+    "NSEC3PARAM",
+    "TLSA",
+    "HIP",
+    "CDS",
+    "CDNSKEY",
+    "CSYNC",
+    "SPF",
+    "UNSPEC",
+    "EUI48",
+    "EUI64",
+    "TKEY",
+    "TSIG",
+    "IXFR",
+    "AXFR",
+    "MAILB",
+    "MAILA",
+    "ANY",
+    "URI",
+    "CAA",
+    "TA",
+    "DLV",
+]
+
+
+@app.route("/", method=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        domain = request.forms.get("domain")
+        print("domain", domain)
+        return redirect("/{0}".format(domain))
+    return template("form.html")
+
+
+@app.get("/<domain>")
+def domain(domain):
+    result = resolve(domain)
+    return template("results.html", result=result)
+
+
+# TODO:
+# 1. Remove http or https if present
+# 2. Validate domain
+def resolve(domain: str):
+    result = []
+    for record in ids:
+        try:
+            answers = dns.resolver.query(domain, record)
+            data = [{"record": record, "value": rdata.to_text()} for rdata in answers]
+            for item in data:
+                result.append(item)
+        except Exception as e:
+            print(e)
+    return result
